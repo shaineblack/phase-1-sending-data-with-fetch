e884dcfe4351bea7cdc03c3ceac8d8e8ba94af57
shaineblack <shaineblck@gmail.com>
2024-02-25 17:52:02 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        const id = data.id;
+        const idElement = document.createElement('p');
+        idElement.textContent = `New ID: ${id}`;
+        document.body.appendChild(idElement);
+      })
+      .catch(function(error) {
+        const errorMessage = error.message;
+        const errorElement = document.createElement('p');
+        errorElement.textContent = `Error: ${errorMessage}`;
+        document.body.appendChild(errorElement);
+      });
+  }
